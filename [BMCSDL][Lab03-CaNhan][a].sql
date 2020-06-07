4fdb107f2c8b9d94050b10c6b34f82d92d686387
Võ Thế Duy <voduy987.tdv@gmail.com>
2020-06-08 01:32:18 +0700

    commit

---
+++ [BMCSDL][Lab03-CaNhan][a].sql
@@ -0,0 +1,18 @@
+﻿/*----------------------------------------------------------
+MASV: 4301104032	
+HO TEN: VO THE DUY
+LAB: 03_a
+NGAY:
+----------------------------------------------------------*/
+
+USE [master]
+GO
+
+IF(DB_ID(N'QLSV') IS NULL)
+BEGIN
+	CREATE DATABASE [QLSV]
+END
+GO 
+
+USE [QLSV]
+GO 		
