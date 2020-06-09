cdf85965f749f53c99d045fbd9ee677d7a53ab8d
Võ Thế Duy <voduy987.tdv@gmail.com>
2020-06-10 01:38:43 +0700

    Change local directory path

---
+++ [BMCSDL][Lab03]/[BMCSDL][Lab03-CaNhan]/[BMCSDL][Lab03-CaNhan][a].sql
@@ -0,0 +1,21 @@
+﻿/*----------------------------------------------------------
+MASV: 4301104032	
+HO TEN: VO THE DUY
+LAB: 03_a
+NGAY:
+----------------------------------------------------------*/
+
+USE [master]
+GO
+
+IF(DB_ID(N'QLSV') IS NOT NULL)
+BEGIN
+	DROP DATABASE [QLSV]
+END
+GO 
+
+CREATE DATABASE [QLSV]
+GO 
+
+USE [QLSV]
+GO 		
