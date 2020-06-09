45595aaa2235d27b1fe31f476c47c740ac1f60d1
Võ Thế Duy <voduy987.tdv@gmail.com>
2020-06-10 00:17:47 +0700

    Add report

--- [BMCSDL][Lab03-CaNhan][d]/[BMCSDL][Lab03-CaNhan][d]/QLSV.Designer.cs
+++ [BMCSDL][Lab03-CaNhan][d]/[BMCSDL][Lab03-CaNhan][d]/QLSV.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\HCMUE\BMCSDL\Lab03\CaNhan\[BMCSDL][Lab03-CaNhan][d]\[BMCSDL][Lab03-CaNhan][d]\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\HCMUE\BMCSDL\Lab03\[BMCSDL][Lab03-CaNhan]\[BMCSDL][Lab03-CaNhan][d]\[BMCSDL][Lab03-CaNhan][d]\QLSV.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
